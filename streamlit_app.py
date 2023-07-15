d5f4b9ea87ffc3032cdca9676cbf002f02dba92c
vtpsakthi <130955385+vtpsakthi@users.noreply.github.com>
2023-07-15 14:32:06 +0530

    Update streamlit_app.py
    Fixed quote issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom''s New Healthy Dinner')
+streamlit.title('My Mom"'"s New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
